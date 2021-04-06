e674a6f30949dee686602641f0ef4dc3f821d39a
灰色飘零 <1418757529@qq.com>
2021-04-06 15:37:08 +0800

    update hello world

--- 4. VSCode FFmpeg Hello World/FFmpegExecute/main.cpp
+++ 4. VSCode FFmpeg Hello World/FFmpegExecute/main.cpp
@@ -1,26 +1,60 @@
 #include "iostream"
+#include "string"
 
 extern "C"
 {
-#include <libavutil/log.h>
+#include "stdio.h"
+#include "libavutil/opt.h"
+#include "libavutil/channel_layout.h"
+#include "libavutil/common.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/time.h"
+#include "libavutil/fifo.h"
+#include "libavdevice/avdevice.h"
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavformat/avio.h"
+#include "libavfilter/avfilter.h"
+#include "libavfilter/buffersink.h"
+#include "libavfilter/buffersrc.h"
+#include "libswscale/swscale.h"
+#include "libswresample/swresample.h"
+#include "libavutil/dict.h"
 }
 
 using namespace std;
 
-void logoutPut(void *ptr, int level, const char *fmt, va_list vl)
+// 声明一下 因为原函数是在.c中声明的结构体
+struct AVDictionary
 {
-    printf("Log Output Content = %s", fmt);
+    int count;
+    AVDictionaryEntry *elems;
+};
+
+AVFormatContext *pFmtCtx = NULL;
+
+void input_dump_format(int index, const char *url, int is_output)
+{
+    int i = 0;
+    int metacount = pFmtCtx->metadata->count;
+    for (i = 0; i < metacount; i++)
+    {
+        cout << "Meta --> " << pFmtCtx->metadata->elems[i].key << ":" << pFmtCtx->metadata->elems[i].value << endl;
+    }
 }
 
 int main(int args, char *argv[])
 {
-    av_log_set_level(AV_LOG_ERROR);
+    // 所有获取音视频信息都要首先注册
+    int errCode = -1;
 
-    av_log_set_callback(logoutPut);
-
-    av_log(NULL, AV_LOG_INFO, "Hello World..FFmpeg \n");
-
-    printf("Hello World.\n");
+    avformat_open_input(&pFmtCtx, "D:/pm.mp4", NULL, NULL);
+    // 官方接口
+    input_dump_format(0, "D:/pm.mp4", 0);
+    // 释放资源
+    avformat_close_input(&pFmtCtx);
 
     system("pause");
 
