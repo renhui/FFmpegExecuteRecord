1537ba31e2fd394c7298c5ca056af9da1b52611c
灰色飘零 <1418757529@qq.com>
2021-04-08 17:16:46 +0800

    update include

--- 7. VSCode FFmpeg Extract Audio/FFmpegExecute/main.cpp
+++ 7. VSCode FFmpeg Extract Audio/FFmpegExecute/main.cpp
@@ -3,25 +3,18 @@
 
 extern "C"
 {
-#include "dshow.h"
 #include "libavutil/opt.h"
-#include "libavutil/channel_layout.h"
-#include "libavutil/common.h"
-#include "libavutil/imgutils.h"
-#include "libavutil/mathematics.h"
-#include "libavutil/samplefmt.h"
-#include "libavutil/time.h"
-#include "libavutil/fifo.h"
-#include "libavdevice/avdevice.h"
-#include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
 #include "libavformat/avio.h"
-#include "libavfilter/avfilter.h"
-#include "libavfilter/buffersink.h"
-#include "libavfilter/buffersrc.h"
-#include "libswscale/swscale.h"
-#include "libswresample/swresample.h"
+#include "libavutil/log.h"
 }
+
+// Copy From libavutil/log.h
+#define AV_LOG_ERROR    16
+#define AV_LOG_WARNING  24
+#define AV_LOG_INFO     32
+
+using namespace std;
 
 void adts_header(char *szAdtsHeader, int dataLen);
 
